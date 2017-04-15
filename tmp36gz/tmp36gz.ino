d7bf54e49cb1176ea3242be78841263aa3c3fa67
KNME <g.giorgobiani@gtu.ge>
2017-04-15 14:00:52 +0400

    Add files via upload

--- tmp36gz/tmp36gz.ino
+++ tmp36gz/tmp36gz.ino
@@ -1,3 +1,19 @@
+#include <DateTime.h>
+#include <SoftwareSerial.h>
+
+const char* day[] =
+{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+const char* month[] =
+{"January", "February", "March", "April", "May", "June", "July", "August","September", "October", "November", "December"};
+
+ byte Hour;
+ byte Minute;
+ byte Second;
+ byte Day;
+ byte DayofWeek; // Sunday is day 0 
+ byte Month;     // Jan is month 0
+ byte Year;      // the Year minus 1900  
+
 int sensorPin = 0; 
  
 void setup()
@@ -7,6 +23,12 @@ void setup()
  
 void loop()                     
 {
+  void serialEvent(Serial myPort)
+{
+ String myString = myPort.readStringUntil('\n');
+ println(myString);
+} 
+ else
   if(Serial.available())
   {
     processSyncMessage();
@@ -16,7 +38,6 @@ void loop()
   else
   digitalClockDisplay();
   delay(1000);
-}
   
  int input = analogRead(sensorPin);  
  float voltage = input * 5.0;
@@ -25,8 +46,6 @@ void loop()
  float temperatureC = (voltage - 0.5) * 100 ;
  Serial.println(" Temperature="); Serial.print(temperatureC);
  
- else
- {
  Serial.println(" Voltage="); Serial.print(voltage); 
  delay(2000);
  }

abcc3f0f2c9393ce112e1e660adaf1eb1cb2f944
KNME <g.giorgobiani@gtu.ge>
2017-04-15 16:07:56 +0400

    Add files via upload

--- tmp36gz/tmp36gz.ino
+++ tmp36gz/tmp36gz.ino
@@ -32,21 +32,22 @@ void loop()
   if(Serial.available())
   {
     processSyncMessage();
-  }
+  {
   if*(timeStatus()==timeNotSet)
   Serial.println("Waiting for syncronization");
-  else
-  digitalClockDisplay();
-  delay(1000);
-  
+  {
+  if*(timeStatus()==timeNotSet) //если время получено.
+  Serial.print("time"); //вывести время.
+  //температура
  int input = analogRead(sensorPin);  
  float voltage = input * 5.0;
  voltage /= 1024.0; 
  
  float temperatureC = (voltage - 0.5) * 100 ;
- Serial.println(" Temperature="); Serial.print(temperatureC);
- 
- Serial.println(" Voltage="); Serial.print(voltage); 
+ Serial.println(" Temperature=");
+ Serial.print(temperatureC);
+ Serial.println(" Voltage=");
+ Serial.print(voltage); 
  delay(2000);
  }
 }

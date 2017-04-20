bf77388e212a65d4c0e2902d8bae22bc382d27be
KNME <g.giorgobiani@gtu.ge>
2017-04-20 04:49:01 +0400

    Add files via upload

---
+++ TEST232/readme.txt
@@ -0,0 +1,8 @@
+1.Compile your code by using the following gcc compiler command:
+
+#type in terminal "cc test232.c".
+
+2.On success compilation, the gcc compiler produces an executable file named
+'a.out' which you will execute using the following command to view the output of your program:
+
+#type in terminal "./a.out".
---
+++ TEST232/test232.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+main()
+{
+int portnum;   //port number.
+int baudrate; //speed of port.
+int bits;    //stop bits. 
+
+char buffer[256];
+printf("|                          Test RS232                     |\n");
+printf("|=========================================================|\n");
+printf("|                    This is test app in c                |\n");
+printf("|=========================================================|\n");
+printf("|                          Sucсessfly!                    |\n");
+while (1);
+printf("                              EXIT                        |\n");
+return (0);
+}

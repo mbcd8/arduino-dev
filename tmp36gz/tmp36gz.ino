68d49be94ff4e2a6740463554416026a01dd3e46
KNME <g.giorgobiani@gtu.ge>
2017-06-24 08:06:35 +0400

    Add files via upload

---
+++ tmp36gz/tmp36gz.ino
@@ -0,0 +1,36 @@
+const int tempPin = 0;
+
+void setup()
+{
+Serial.begin(9600);
+}
+
+void loop()
+{
+static int line = 0;
+  
+line = line + 1;
+float temp, volt;
+volt = pinVoltage(tempPin);
+temp = (volt - 0.5) * 100.0;
+  
+Serial.print("  line: ");
+Serial.print(line);
+Serial.print(" | ");
+
+Serial.print("temperature: ");
+Serial.print(temp);
+Serial.print(" | ");
+
+Serial.print("voltage: ");
+Serial.print(volt);
+Serial.println(" | ");
+  
+delay(2000);
+}
+
+float pinVoltage (int pin)
+
+{
+return (analogRead(pin) * 0.004882814);
+}
